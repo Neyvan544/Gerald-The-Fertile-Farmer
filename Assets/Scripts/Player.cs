6fa8a2aa30c184fc6e7b50b9bc4f873592b28044
Neyvan544 <vanya.kostyukevich.00@gmail.com>
2024-01-10 13:55:54 +0200

    Interaction(Plowed field placed)
    Added interaction with ground
    Need to fix position of place plowed field.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,15 +14,30 @@ public class Player : MonoBehaviour
         
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        { 
+           Vector3Int position = new Vector3Int((int)transform.position.x, 
+               (int)transform.position.y, 0); 
+            
+            if(GameManager.instance.tileManager.IsInteractable(position)) 
+            {
+                Debug.Log("Tile is interactable");
+                GameManager.instance.tileManager.SetInteracted(position);
+            }
+        }
+    }
+
     public void DropItem(Collectable item)
     {
        
-        Vector3 spawnLocation = transform.position;
+        Vector2 spawnLocation = transform.position;
 
-        Vector3 spawnOffset = Random.insideUnitCircle * 1.25f;
+        Vector2 spawnOffset = Random.insideUnitCircle * 1.5f;
 
         Collectable droppedItem = Instantiate(item, spawnLocation + spawnOffset, 
             Quaternion.identity);
-        droppedItem.rb2d.AddForce(spawnOffset * 2f, ForceMode2D.Impulse);
+        droppedItem.rb2d.AddForce(spawnOffset * .2f, ForceMode2D.Impulse);
     }
 }
